9ba84aa632af7639abb5b12064a35baa6bb6249f
Bruno Ribeiro Chaves <bruno.chaves@email.com>
2025-07-11 11:31:09 -0300

    codigo com telegram tambem

---
+++ MoCaFri_scketch/arduino_secrets.h
@@ -0,0 +1,19 @@
+//Token Arduino Cloud
+#define SECRET_DEVICE_KEY "4zaqMGCGnUgNLQLZq@r1?N?WH"
+
+//Dados Wifi
+#define SECRET_OPTIONAL_PASS "andredfc9"
+#define SECRET_SSID "Andredfc"
+
+//Token do bot
+#define BOT_TOKEN "7928889627:AAHZ61dNebFslesT4Mdu2b1rU5fIDeNtdKQ"
+
+//Id do usuário do telegram que irá receber as mensagens
+#define CHAT_ID "1475425161"
+
+//Faixa de temperatura aceitavel para alarmes
+#define MIN_TEMPERATURE 10
+#define MAX_TEMPERATURE 30
+
+//Tempo maximo em minutos de porta aberta
+#define MAX_DOOR_TIME_SEC 10
